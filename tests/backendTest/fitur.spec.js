272fe1c3f742c98bb6d3907439e7508075e9a799
Computer Nusapala <ismaya.bimmo@gmail.com>
2026-02-06 14:18:03 +0700

    enh-get-post-for-feature-page

---
+++ tests/backendTest/fitur.spec.js
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+import { request } from 'http';
+
+const BASE_URL = 'https://main-engine-staging.nusapalaparking.com';
+
+test.describe('Backend API Testing - Fitur', () => {
+
+    test('Sistem mengambil data sesuai urutan (GET)', async ({ request }) => {
+        const response = await request.get(`${BASE_URL}/api/v1/main-engine/features`);
+        expect(response.status()).toBe(200);
+
+        const body = await response.json();
+        console.log('Respon API:', JSON.stringify(body, null, 2));
+
+    });
+
+    test('Sistem get fitur code bervalue Modular', async ({ request }) => {
+        const response = await request.get(`${BASE_URL}/api/v1/main-engine/features`);
+        const body = await response.json();
+
+        const findFitur = body.data.find(item => item.feature_code === 'MODULAR');
+        expect(findFitur).toBeDefined();
+
+        expect(findFitur.feature_code).toBe('MODULAR');
+
+        expect(findFitur.feature_active).toBe(true);
+    });
+
+    test('Sistem buat fitur baru', async ({ request }) => {
+        const response = await request.post(`${BASE_URL}/api/v1/main-engine/features`, {
+            data: {
+                feature_active: true,
+                feature_code: "AUTO002",
+                feature_description: "Test-menggunakan-automation",
+                feature_name: "Auto002",
+                parent_feature_id: null
+
+            }
+        });
+
+
+        const body = await response.json();
+        console.log('Respon POST:', JSON.stringify(body, null, 2));
+        expect(response.status()).toBe(201);
+
+    });
+})
